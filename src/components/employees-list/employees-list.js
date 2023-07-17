265ef85379d479a5de6f1b5dc76f1a5d535a8809
Svyatoslav <slavaanufriev@gmail.com>
2023-07-17 20:22:18 +0300

    added props in employeesList

--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -4,9 +4,9 @@ import './employees-list.css';
 const EmployeesList = () => {
     return (
         <ul className="app-list list-group">
-            <EmployeesListItem/>
-            <EmployeesListItem/>
-            <EmployeesListItem/>
+            <EmployeesListItem name="Алексеев А.В" salary={800}/>
+            <EmployeesListItem name="Бродников В.А" salary={3000}/>
+            <EmployeesListItem name="Исаев И.К" salary={5000}/>
         </ul>
     )
 }
